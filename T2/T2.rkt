594ea400025acd3abea2a467afa54fe9a6530018
Victor <axiomateorico@gmail.com>
2024-09-21 19:22:07 -0300

    avance t2

--- T2/T2.rkt
+++ T2/T2.rkt
@@ -50,10 +50,10 @@ Concrete syntax of propositions:
 <s-prop> ::= true
           | false
           |<sym>
-          |(list 'not <s-expr>)
-          |(list 'and <s-expr>)
-          |(list 'or <s-expr>)
-          |(list '<s-expr> where(list <sym> <s-expr>)) 
+          |(list 'not <s-prop>)
+          |(list 'and <s-prop>)
+          |(list 'or <s-prop>)
+          |(list '<s-prop> where(list <sym> <s-prop>)) 
 |#
 
 ;; parse-prop : <s-prop> -> Prop
@@ -83,13 +83,21 @@ Concrete syntax of propositions:
 
 
 #|
-<value> ::= ...
+<pvalue> ::= (ttV)
+           | (ffV)
 |#
+(deftype PValue
+  (ttV)
+  (ffV))
 
-;; (deftype PValue ...)
+
 
 ;; from-Pvalue : PValue -> Prop
-(define (from-Pvalue p-value) '???)
+(define (from-PValue pv)
+  (match pv
+    [(ttV) (tt)]
+    [(ffV) (ff)]))
+
 
 
 ;;----- ;;
@@ -98,7 +106,18 @@ Concrete syntax of propositions:
 
 
 ;; p-subst : Prop Symbol Prop -> Prop
-(define (p-subst target name substitution) '???)
+(define (p-subst target name substitution)
+  (match target
+    [(tt) (tt)]
+    [(ff) (ff)]
+    [(p-id s) (if (eq? s name) substitution target)]
+    [(p-not n) (p-not (p-subst n name substitution))]
+    [(p-and a) (p-and (map (lambda (p) (p-subst p name substitution)) a))]
+    [(p-or o) (p-or (map (lambda (p) (p-subst p name substitution)) o))]
+    [(p-where p i q) 
+     (if (eq? i name)
+         (p-where p i (p-subst q name substitution))
+         (p-where (p-subst p name substitution) i (p-subst q name substitution)))]))
 
 
 ;;----- ;;
@@ -106,14 +125,41 @@ Concrete syntax of propositions:
 ;;----- ;;
 
 
+
 ;; eval-or : (Listof Prop) -> PValue
-(define (eval-or ps) '???)
+(define (eval-or ps)
+  (match ps
+    ['() (ffV)] 
+    [(cons p rest)
+     (let ([p-value (p-eval p)])
+       (if (equal? p-value (ttV))
+           (ttV) 
+           (eval-or rest)))]))
+
 
 ;; eval-and : (Listof Prop) -> PValue
-(define (eval-and ps) '???)
+(define (eval-and ps)
+  (match ps
+    ['() (ttV)]  
+    [(cons p rest)
+     (let ([p-value (p-eval p)])
+       (if (equal? p-value (ffV))
+           (ffV) 
+           (eval-and rest)))]))
 
 ;; p-eval : Prop -> PValue
-(define (p-eval p) '???)
+(define (p-eval p)
+  (match p
+    [(tt) (ttV)]
+    [(ff) (ffV)]
+    [(p-not n) (if (equal? (p-eval n) (ttV)) (ffV) (ttV))]
+    [(p-and a) (eval-and a)]
+    [(p-or o) (eval-or o)]
+    [(p-where p i q) 
+     (let ([q-value (p-eval q)])
+       (p-eval (p-subst p i (from-PValue q-value))))]
+    [(p-id s) (error (format "Unbound variable: ~a" s))]))
+
 
 ;;------------ ;;
 ;;==== P2 ==== ;;
@@ -123,20 +169,23 @@ Concrete syntax of propositions:
 ;; P2.a ;;
 ;;----- ;;
 
-
 #|
-<expr> ::= ...
+<expr> ::= (real <num>)
+        | (id <sym>)
+        | (imaginary <num>)
         | (add <expr> <expr>)
         | (sub <expr> <expr>)
         | (if0 <expr> <expr> <expr>)
-        | ...
+        | (with [((<sym> <expr>))*] <expr>)
 |#
 (deftype Expr
-  ; ...
+  (real r)
+  (imaginary i)
+  (id x)
   (add l r)
   (sub l r)
   (if0 c t f)
-  ; ...
+  (with bindings body)
   )
 
 ;;----- ;;
@@ -146,26 +195,40 @@ Concrete syntax of propositions:
 #|
 Concrete syntax of expressions:
 
-<s-expr> ::= ...
+<s-expr> ::= <num>
+        | (cons <num> i)
         | (+ <s-expr> <s-expr>)
         | (- <s-expr> <s-expr>)
         | (if0 <s-expr> <s-expr> <s-expr>)
-        | ...
+        | (list 'with (list <sym> <s-expr>) <s-expr>)
+        | <sym>
 |#
 
 ;; parse : <s-expr> -> Expr
+(define (parse s-expr)
+  (match s-expr
+    [(? number?) (real s-expr)]  
+    [(list n i) (imaginary n)]  
+    [( list '+ l r) (add (parse l) (parse r))] 
+    [(list '- l r) (sub (parse l) (parse r))]  
+    [(list 'if0 c t f) (if0 (parse c) (parse t) (parse f))] 
+    [(list 'with bindings body)
+     (if (null? bindings)
+         (error "parse: 'with' expects at least one definition")
+         (let* ([parsed-bindings (map (lambda (b) 
+                                        (match b
+                                          [(list id expr) (cons id (parse expr))]
+                                          [_ (error "Invalid binding in 'with'")]))
+                                      bindings)])
+           (with parsed-bindings (parse body))))]
 
-(define (parse s-expr) '???)
+    [(? symbol?) (id s-expr)] 
+    [_ (error "Invalid expression" s-expr)])) 
+
+
 
 ;;----- ;;
 ;; P2.c ;;
-;;----- ;;
-
-;; subst :: Expr Symbol Expr -> Expr
-(define (subst in what for) '???)
-
-;;----- ;;
-;; P2.d ;;
 ;;----- ;;
 
 #|
@@ -175,21 +238,106 @@ Concrete syntax of expressions:
 (deftype CValue (compV r i))
 
 ;; from-CValue :: CValue -> Expr
-(define (from-CValue v) '???)
+(define (from-CValue v)
+  (match v
+      [(compV r i)
+       (cond [(= i 0) (real r)]
+             [(= r 0) (imaginary )]
+             [else (add (real r) (imaginary i) )])]))
+
+
 
 ;; cmplx+ :: CValue CValue -> CValue
-(define (cmplx+ v1 v2) '???)
+(define (cmplx+ v1 v2)
+       (match (list v1 v2)
+         [(list (compV r1 i1) (compV r2 i2))   (compV (+ r1 r2) (+ i1 i2)) ]))
 
 ;; cmplx- :: CValue CValue -> CValue
-(define (cmplx- v1 v2) '???)
+(define (cmplx- v1 v2)
+       (match (list v1 v2)
+         [(list (compV r1 i1) (compV r2 i2))   (compV (- r1 r2) (- i1 i2)) ]))
+
 
 ;; cmplx0? :: CValue -> Boolean
-(define (cmplx0? v) '???)
+(define (cmplx0? v)
+  (match v
+     [(compV r i)
+    (if ( and (= r 0) (= i 0)) #t #f) ]))
 
 
+
+;;----- ;;
+;; P2.d ;;
+;;----- ;;
+
+
+;; aux-subst :: [((<sym> <expr>))*] Symbol Expr -> [((<sym> <expr>))*]
+(define (aux-subst bindings what for)
+  (match bindings
+    ['() '()]
+    [(cons (cons id val) rest-bindings)
+     (if (shadowing? bindings what)
+         (cons (cons id val) rest-bindings)
+         (cons (cons id (subst val what for))
+               (aux-subst rest-bindings what for)))]))
+
+
+;; shadowing? :: [((<sym> <expr>))*] Symbol  -> Boolean
+(define (shadowing? bindings what)
+  (match bindings
+    ['() #f]
+    [(cons (cons id _) rest-bindings)
+     (if (eq? id what)
+         #t
+         (shadowing? rest-bindings what))]))
+
+
+
+;; subst :: Expr Symbol Expr -> Expr
+(define (subst expr what for)
+  (match expr
+    [(real r) (real r)]
+    [(imaginary i) (imaginary i)]
+    [(add l r) (add (subst l what for) (subst r what for))]
+    [(sub l r) (sub (subst l what for) (subst r what for))]
+    [(if0 c t f) (if0 (subst c what for)
+                      (subst t what for)
+                      (subst f what for))]
+    [(id x)
+     (if (eq? x what)
+         for
+         (id x))]
+    [(with bindings body)
+     (let ([new-bindings (aux-subst bindings what for)])
+       (with new-bindings
+             (if (shadowing? new-bindings what)
+                 body  
+                 (subst body what for))))]))
+
+
+                                          
 ;;----- ;;
 ;; P2.e ;;
 ;;----- ;;
 
 ;; interp : Expr -> CValue
-(define (interp expr) '???)
+(define (interp expr)
+    (match expr
+    [(real r) (compV r 0)]
+    [(imaginary i) (compV 0 i)]
+    [(id x) (error 'interp "Open expression (free occurrence of ~a)" x)]
+    [(add l-expr r-expr) (cmplx+ (interp l-expr) (interp r-expr))]
+    [(sub l-expr r-expr) (cmplx- (interp l-expr) (interp r-expr))]
+    [(if0 c-expr t-expr f-expr)  (if (cmplx0? (interp c-expr))
+         (interp t-expr)
+         (interp f-expr))]
+    [(with bindings body)
+          (interp  (from-CValue (fold-bindings bindings body)))]))
+  
+  
+(define (fold-bindings bindings body)
+  (match bindings
+    ['() body]
+    [(cons (cons id expr) rest-bindings)
+     (let ([new-body (subst body id (interp expr))])
+       (fold-bindings rest-bindings new-body))]))
--- T2/test.rkt
+++ T2/test.rkt
@@ -3,6 +3,7 @@
 
 (print-only-errors #t)
 
+;;P1
 (test (parse-prop 'x) (p-id 'x))
 (test (parse-prop '(and true false true)) (p-and ( list (tt) ( ff ) (tt))))
 (test (parse-prop ' (not true)) (p-not (tt)))
@@ -11,3 +12,63 @@
 (test (parse-prop ' (x where [x true] )) (p-where (p-id 'x) 'x (tt)) )
 (test/exn (parse-prop ' (and true)) "parse-prop: and expects at least two operands")
 (test/exn (parse-prop ' (or)) "parse-prop: or expects at least two operands")
+
+(test (from-PValue (ttV))(tt))
+(test (from-PValue (ffV))(ff))
+
+(test (p-subst (p-id 'x) 'x (tt)) (tt))
+(test (p-subst (p-id 'x) 'y (tt)) (p-id 'x))
+(test (p-subst (p-where (p-id 'x) 'x (tt)) 'x ( ff )) (p-where (p-id 'x) 'x (tt)))
+(test (p-subst (p-where (p-id 'x) 'y (tt)) 'x ( ff )) (p-where (ff) 'y (tt)))
+
+
+(test (p-eval (tt)) (ttV))
+(test (p-eval (ff)) (ffV))
+(test (p-eval (p-not (tt))) (ffV))
+(test (p-eval (p-not (ff))) (ttV))
+(test (p-eval (p-not (p-not (tt)))) (ttV) )
+(test (p-eval (p-and (list (tt) (tt) (tt)))) (ttV))
+(test (p-eval (p-and (list (tt) (ff) (tt)))) (ffV))
+(test (p-eval (p-or (list (ff) (ff) (ff)))) (ffV))
+(test (p-eval (p-or (list (ff) (tt) (ff)))) (ttV))
+(test (p-eval (p-where (p-id 'x) 'x (tt))) (ttV))
+(test (p-eval (p-and (list (tt) (p-or (list (ff) (tt))) (p-not (ff))))) (ttV))
+(test (p-eval (p-and (list (tt) (p-or (list (ff) (p-and (list (tt) (p-not (ff))))))))) (ttV) )
+(test (p-eval (p-and (list (tt)))) (ttV))
+(test/exn (p-eval (p-id 'x)) "Unbound variable: x")
+(test/exn (p-eval (p-and (list (tt) (p-id 'x)))) "Unbound variable: x")
+(test (p-eval (p-where (p-where (p-or (list (p-id 'x) (p-id 'y))) 'y (ff)) 'x (tt))) (ttV))
+
+;;P2
+(test (parse '1) ( real 1))
+(test (parse ' (1 i )) (imaginary 1))
+(test (parse ' (+ 1 (2 i ))) (add (real 1) (imaginary 2)))
+(test (parse ' (with [(x 1) (y 1)] (+ x y)))  (with ( list (cons 'x ( real 1)) (cons 'y ( real 1))) (add (id 'x) (id 'y))) )
+(test/exn (parse ' (with [ ] 1)) "parse: 'with' expects at least one definition") 
+
+
+(test (from-CValue (compV 1 0 )) (real 1))
+(test (from-CValue(compV 2 2)) (add (real 2) (imaginary 2)))
+(test (from-CValue(compV  0 0 )) (real 0))
+
+
+(test (cmplx+ (compV 1 2) (compV 3 4)) (compV 4 6))
+(test (cmplx- (compV 1 2) (compV 3 4)) (compV -2 -2))
+(test (cmplx0? (compV 0 1)) #f)
+(test (cmplx0? (compV 0 0)) #t)
+
+
+(test (subst (parse ' (with [(x 2) (y z) ] (+ x z))) 'z ( real 1))  (with ( list (cons 'x ( real 2)) (cons 'y ( real 1))) (add (id 'x) ( real 1))))
+(test (subst (parse ' (with [(x 2) (y x)] (+ x x))) 'x ( real 1)) (with ( list (cons 'x ( real 2)) (cons 'y (id 'x))) (add (id 'x) (id 'x))) )
+(test (subst (parse ' (with [(y x) (x 2)] (+ x x))) 'x ( real 1)) (with ( list (cons 'y ( id 'x)) (cons 'x (real 2))) (add (id 'x) (id 'x))) )
+(test (subst (parse ' (with [(z 2) (y x)] (+ x x))) 'x ( real 1)) (with ( list (cons 'z ( real 2)) (cons 'y (real 1))) (add (real 1) (real 1))) )
+
+
+(test (interp (real 5)) (compV 5 0))
+(test (interp (imaginary 2)) (compV 0 2))
+(test (interp (add (real 1) (imaginary 1))) (compV 1 1))
+(test (interp (with (list (cons 'x (real 5))) (id 'x)))  (compV 5 0))
+(test (interp (with (list (cons 'x (real 5)) (cons 'y (add (id 'x) (real 3)))) (add (id 'x) (id 'y)))) (compV 13 0))
+
+
+
